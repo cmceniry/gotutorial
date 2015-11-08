983add8602158435e493ee433d57320e72d225df
Chris McEniry <cmceniry@mit.edu>
2015-11-07 19:07:34 -0800

    * simple channel exercise

---
+++ time-stat/README.md
@@ -0,0 +1,31 @@
+time-stat
+=========
+
+This exercise goes over simple channel usage. You'll be reproducing
+some behavior of the time.Tick standard library call, but it's a good
+example to cover.
+
+The goal is to output "uptime" results every second.
+
+You will need to reference the time standard library for other functions:
+
+  https://golang.org/pkg/time/
+
+Focus - Tutorial Section
+------------------------
+channel
+select
+case
+
+Prerequisite - Tutorial Section
+-------------------------------
+gettingstarted
+func
+if
+panic
+err
+for
+
+Setup
+-----
+None
---
+++ time-stat/answer/stat.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"os/exec"
+)
+
+// The Uptime function just runs uptime and returns the output
+func Uptime() string {
+	out, err := exec.Command("uptime").Output()
+	if err != nil {
+		return "Unable to run uptime\n"
+	}
+	return string(out)
+}
+
+// Snoozer's  sends the current time to another goroutine every second.
+// The presence of data on that channel is intended to indicate to that
+// other goroutine that it's time to do work.
+func Snoozer(c chan time.Time) {
+	for {
+		time.Sleep(time.Second)
+		// Attempt to send time.Now to the c chan, but don't wait for it.
+		// If we wait for it, we might be stuck for an indefinite amount of
+		// time and fail at our goal of sending it every second.
+		select {
+		case c <- time.Now():
+		default:
+		}
+	}
+}
+
+func main() {
+	// Create our channel for Snoozer to user
+	t := make(chan time.Time)
+	// Start up Snoozer
+	go Snoozer(t)
+	for {
+		// Get data from Snoozer, or wait if Snoozer hasn't said it's time yet
+		ct := <- t
+		// Output current time, and uptime output
+		fmt.Printf("%s : %s", ct, Uptime())
+	}
+}
