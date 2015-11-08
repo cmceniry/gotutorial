dbf87cde7866d8449e08c6f65a24ae9819f4db87
Chris McEniry <cmceniry@mit.edu>
2015-11-07 22:09:54 -0800

    * sscanf exercise to start on pointers

---
+++ scan/README.md
@@ -0,0 +1,26 @@
+scan
+====
+
+The fmt Scanf commands are used to parse formatted datasets. It parses data
+using the fmt formats, and puts those into the Go data structures you
+specific. You'll be using
+
+  https://golang.org/pkg/fmt/
+
+Scanf relies on pointers to pass data around.
+
+Focus - Tutorial Section
+------------------------
+pointers
+
+Prerequisite - Tutorial Section
+-------------------------------
+gettingstarted
+if
+err
+panic
+for
+
+Setup
+-----
+None
---
+++ scan/scan.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+  "fmt"
+  "io"
+  "io/ioutil"
+  "strings"
+)
+
+func main() {
+  data, err := ioutil.ReadFile("sample.dat")
+  if err != nil {
+    panic(err)
+  }
+
+  // The data format is a string followed by 5 integers
+  dformat := "%s %d %d %d %d %d"
+  // temporary locations for holding data
+  tmpname := ""
+  tmpdata := [5]int{}
+
+  // Split the data by lines, and process it
+  for _, line := range strings.Split(string(data), "\n") {
+    // Sscanf the line and load it into the tmpvalues
+    n, err := ...
+    // panic if there are io errors (which aren't EOF)
+    if err != nil && err != io.EOF {
+      panic(err)
+    }
+    // If we don't scan enough fields, assume that we're at the end of
+    // file and break out
+    if n != 6 {
+      break
+    }
+
+    // Compute the average and display with the dataset name
+    total := 0
+    for _, v := range tmpdata {
+      total += v
+    }
+    fmt.Printf("%s : avg=%d\n", tmpname, total/5)
+  }
+
+}
